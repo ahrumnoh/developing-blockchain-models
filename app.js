136c9854fb38f17e27146f660e60b7ae21e82ac6
Rose Noh <sydneyroseenglish@gmail.com>
2022-07-19 16:50:42 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+const Block = require('./block')
+const Blockchain = require('./blockchain')
+
+let genesisBlock = new Block()
+let blockchain = new Blockchain(genesisBlock)
+
+console.log(blockchain)
