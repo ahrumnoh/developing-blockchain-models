724b696090e309a4f544ffc2e5251473d313edd5
Rose Noh <sydneyroseenglish@gmail.com>
2022-07-25 16:34:14 +0900

    update
    update

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
+
+
 const Block = require('./block')
 const Blockchain = require('./blockchain')
-const Transaction = require('./transaction')
-
-let transaction = new Transaction('Rose', 'Great', 100)
 
 let genesisBlock = new Block()
 let blockchain = new Blockchain(genesisBlock)
 
-let block = blockchain.getNextBlock([transaction])
-blockchain.addBlock(block)
-
-<<<<<<< Updated upstream
-let anotherTransaction = new Transaction('steven', "Brian", 500)
-let block1 = blockchain.getNextBlock.getNextBlock([anotherTransaction, transaction])
-blockchain.addBlock(block1)
-
 console.log(blockchain)
-=======
-console.log(blockchain)
->>>>>>> Stashed changes
--- blockchain.js
+++ blockchain.js
@@ -7,7 +7,7 @@ const Transaction = require('./transaction')
 class Blockchain {
     constructor(genesisBlock) {
         this.blocks = []
-        this.addBlock(genesisBlock) //
+        this.addBlock(genesisBlock)
     }
 
     addBlock(block) {
@@ -17,30 +17,8 @@ class Blockchain {
         }
 
         this.blocks.push(block)
-    } //
-
-    getNextBlock([transactons]) {
-        let block = new Block()
-        transactions.forEach((transaction) => {
-            block.addTransaction(transaction)
-        })
-
-        let previousBlock = this.getPreviousBlock()
-        block.index = this.blocks.length
-        block.previousHash = previousBlock.hash
-        block.hash = this.generateHash(block)
-
-        return block
     }
 
-<<<<<<< Updated upstream
-    getPreviousBlock() { //
-        return this.blocks[this.blocks.length - 1]
-    }
-
-
-
-=======
     getNextBlock(transactions) {
         let block = new Block()
         transactions.forEach((transaction) => {
@@ -58,26 +36,16 @@ class Blockchain {
     getPreviousBlock() {
         return this.blocks[this.blocks.length - 1]
     }
->>>>>>> Stashed changes
 
     generateHash(block) {
         let hash = sha256(block.key)
 
         while(!hash.startsWith('0000')) {
             block.nonce += 1
-<<<<<<< Updated upstream
-            hash = sha256(block.hey)
-            console.log(hash)
-        }
-
-
-
-=======
             hash = sha256(block.key)
             console.log(hash)
 
         }
->>>>>>> Stashed changes
         return hash
     }
 }
