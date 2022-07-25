b243b71264eb4b1f7bc94c9aa2e7a8bb1e955ecd
Rose Noh <sydneyroseenglish@gmail.com>
2022-07-25 16:45:47 +0900

    update
    updated changes

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 
 const Block = require('./block')
 const Blockchain = require('./blockchain')
+const Transaction = require('./transaction')
+
+let transaction = new Transaction('Rose', 'Great', 100)
 
 let genesisBlock = new Block()
 let blockchain = new Blockchain(genesisBlock)
 
+let block = blockchain.getNextBlock([transaction])
+blockchain.addBlock(block)
 console.log(blockchain)
