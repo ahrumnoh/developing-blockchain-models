3149ecf6783b840b5523f2bae32edb1b235ca727
ahrum noh <sydneyroseenglish@gmail.com>
2022-07-29 14:32:46 +0900

    update
    changes

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-
-const express = require('express')
-const app = express()
-
 const Block = require('./block')
 const Blockchain = require('./blockchain')
 const Transaction = require('./transaction')
 
-let transaction = new Transaction('Rose', 'Great', 100)
 
-let genesisBlock = new Block()
-let blockchain = new Blockchain(genesisBlock)
+const express = require('express')
+const app = express()
 
-let block = blockchain.getNextBlock([transaction])
-blockchain.addBlock(block)
+app.get('/blockchain', (req, res) => {
 
-let anotherTransaction = new Transaction('Patrick', 'James', 500)
-let block1 = blockchain.getNextBlock([anotherTransaction, transaction])
-blockchain.addBlock(block1)
+    
+    let transaction = new Transaction('Rose', 'Great', 100)
+
+    let genesisBlock = new Block()
+    let blockchain = new Blockchain(genesisBlock)
+
+    let block = blockchain.getNextBlock([transaction])
+    blockchain.addBlock(block)
+
+    let anotherTransaction = new Transaction('Patrick', 'James', 500)
+    let block1 = blockchain.getNextBlock([anotherTransaction, transaction])
+    blockchain.addBlock(block1)
 
 
-console.log(blockchain)
+    res.json(blockchain)
+
+})
+
+
+app.listen (8080, () => {
+    console.log('Server is running ...')
+})
+
+
+
+
+// console.log(blockchain)
